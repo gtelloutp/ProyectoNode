2e2b652f35b82a3653fc43fd80ca5d9f320a9096
gtelloutp <utp0159380@alumno.utpuebla.edu.mx>
2025-04-10 17:20:29 -0600

    Update uber.route.js

--- MAD/backend-app/routes/uber.route.js
+++ MAD/backend-app/routes/uber.route.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const {
-    getUber,
-    getUberById,
-    setUber,
-    updateUber,
-    deleteUberById
+    getUberDataset,
+    getUberDatasetById,
+    setUberDataset,
+    updateUberDataset,
+    deleteUberDatasetById
 } = require('../controllers/uber.controller')
 
 //Endpoint para recuperar toda la coleccion de Ubers
-router.get('/all', getUber)
+router.get('/all', getUberDataset)
 //Endpoint para recuperar un Uber por su id
-router.get('/ById/:id', getUberById)
+router.get('/ById/:id', getUberDatasetById)
 //Endpoint para crear un nuevo Uber
-router.post('/add', setUber)
+router.post('/add', setUberDataset)
 //Endpoint para actualizar un Uber
-router.put('/update/:id', updateUber)
+router.put('/update/:id', updateUberDataset)
 //Endpoint para eliminar un Uber
-router.delete('/delete/:id', deleteUberById)
+router.delete('/delete/:id', deleteUberDatasetById)
 
 module.exports = router
